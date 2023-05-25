1b56c25b6d8e38bed9f7305f925b56c7142cb051
thiagocontelli <thiagocontellid@gmail.com>
2023-05-25 15:09:00 -0300

    refactor: add lombok to Product model

--- src/main/java/com/tc/stockcontrol/product/Product.java
+++ src/main/java/com/tc/stockcontrol/product/Product.java
@@ -1,25 +1,20 @@
 package com.tc.stockcontrol.product;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table
-public record Product(
-        @Id
-        @SequenceGenerator(
-            name = "product_sequence",
-            sequenceName = "product_sequence",
-            allocationSize = 1
-        )
-        @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "product_sequence"
-        )
-        Long id,
-        String name,
-        Double price,
-        Integer category,
-        Integer quantity,
-        Long expirationDate,
-        Long createdAt
-) { }
+public class Product {
+    @Id
+    @SequenceGenerator(name = "product_sequence", sequenceName = "product_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence")
+    Long id;
+    String name;
+    Double price;
+    Integer category;
+    Integer quantity;
+    Long expirationDate;
+    Long createdAt;
+}
