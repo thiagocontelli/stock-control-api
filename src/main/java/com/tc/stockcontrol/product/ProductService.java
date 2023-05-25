1c9a7a5d44cbd932736f0a690348d8375967da78
thiagocontelli <thiagocontellid@gmail.com>
2023-05-25 13:20:09 -0300

    feat: create ProductService

---
+++ src/main/java/com/tc/stockcontrol/product/ProductService.java
@@ -0,0 +1,22 @@
+package com.tc.stockcontrol.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> list() {
+        return productRepository.findAll();
+    }
+
+}
