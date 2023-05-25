1c8f070d2a7b69f118d6a6870bb517afc9cbf130
thiagocontelli <thiagocontellid@gmail.com>
2023-05-25 20:35:31 -0300

    refactor: the database properties of Product model

--- src/main/java/com/tc/stockcontrol/product/Product.java
+++ src/main/java/com/tc/stockcontrol/product/Product.java
@@ -2,19 +2,36 @@ package com.tc.stockcontrol.product;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import java.util.Date;
 
 @Data
 @Entity
 @Table
 public class Product {
     @Id
-    @SequenceGenerator(name = "product_sequence", sequenceName = "product_sequence", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence")
-    Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    @Column(name = "name", nullable = false)
     String name;
+
+    @Column(name = "price", nullable = false)
     Double price;
+
+    @Column(name = "category", nullable = false)
     Integer category;
+
+    @Column(name = "quantity", nullable = false)
     Integer quantity;
-    Long expirationDate;
-    Long createdAt;
+
+    @Column(name = "expiration_date", nullable = false)
+    Date expirationDate;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    Date createdAt;
+
+    @Column(name = "bar_code", nullable = false)
+    String barCode;
 }
