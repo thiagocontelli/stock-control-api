2061ccb1e04399b4ad93a971d4bf85ae7669fd86
thiagocontelli <thiagocontellid@gmail.com>
2023-05-25 13:18:53 -0300

    feat: create Product model

---
+++ src/main/java/com/tc/stockcontrol/product/Product.java
@@ -0,0 +1,25 @@
+package com.tc.stockcontrol.product;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public record Product(
+        @Id
+        @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 1
+        )
+        @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+        )
+        Long id,
+        String name,
+        Double price,
+        Integer category,
+        Integer quantity,
+        Long expirationDate,
+        Long createdAt
+) { }
