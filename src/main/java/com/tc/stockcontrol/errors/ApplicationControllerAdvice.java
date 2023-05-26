6daebc79d4575a18c06bdb6ea9662f2977d2c12e
thiagocontelli <thiagocontellid@gmail.com>
2023-05-26 20:17:15 -0300

    feat: add error handling to bean error

--- src/main/java/com/tc/stockcontrol/errors/ApplicationControllerAdvice.java
+++ src/main/java/com/tc/stockcontrol/errors/ApplicationControllerAdvice.java
@@ -2,8 +2,11 @@ package com.tc.stockcontrol.errors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -23,6 +26,12 @@ public class ApplicationControllerAdvice {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleBadRequestException(BadRequestException e) {
         errorResponse.setMessage(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        errorResponse.setMessage((Objects.requireNonNull(e.getFieldError()).getField() + " " + e.getFieldError().getDefaultMessage()).replaceAll(" ", "_"));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
