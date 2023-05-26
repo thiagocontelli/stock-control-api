7e8875ab95c9ec2e6b82ad781844bb627a8f9af7
thiagocontelli <thiagocontellid@gmail.com>
2023-05-26 12:45:30 -0300

    refactor: change the Product id type in ProductRepository

--- src/main/java/com/tc/stockcontrol/product/ProductRepository.java
+++ src/main/java/com/tc/stockcontrol/product/ProductRepository.java
@@ -2,5 +2,5 @@ package com.tc.stockcontrol.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, String> {
 }
