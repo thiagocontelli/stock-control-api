dd19d13e1efe8f3af5bac9e434bb87211d1b4f74
thiagocontelli <thiagocontellid@gmail.com>
2023-05-25 22:14:52 -0300

    feat: create validation to dto properties in ProductService add method

--- src/main/java/com/tc/stockcontrol/product/ProductService.java
+++ src/main/java/com/tc/stockcontrol/product/ProductService.java
@@ -1,12 +1,14 @@
 package com.tc.stockcontrol.product;
 
-import com.tc.stockcontrol.dtos.product.ProductReqMapper;
 import com.tc.stockcontrol.dtos.product.ProductReqDTO;
+import com.tc.stockcontrol.dtos.product.ProductReqMapper;
 import com.tc.stockcontrol.dtos.product.ProductResDTO;
 import com.tc.stockcontrol.dtos.product.ProductResMapper;
+import com.tc.stockcontrol.errors.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,6 +36,28 @@ public class ProductService {
     }
 
     public ProductResDTO add(ProductReqDTO dto) {
+        if (dto.name().trim().isEmpty()) {
+            throw new BadRequestException("name_cant_be_empty");
+        }
+
+        if (dto.price() < 0) {
+            throw new BadRequestException("price_cant_be_negative");
+        }
+
+        if (dto.quantity() < 0) {
+            throw new BadRequestException("quantity_cant_be_negative");
+        }
+
+        if (dto.barCode().trim().isEmpty()) {
+            throw new BadRequestException("bar_code_cant_be_empty");
+        }
+
+        if (dto.expirationDate().toInstant().isBefore(Instant.now())) {
+            throw new BadRequestException("expiration_date_cant_be_in_the_past");
+        }
+
+        // TODO: find a way to validate category property
+
         Product newProduct = productReqMapper.toEntity(dto);
         Product product = productRepository.save(newProduct);
         return productResMapper.toDTO(product);
