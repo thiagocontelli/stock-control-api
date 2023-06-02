8266edaa3c1b839ce3ce700db786bdff00cc8636
thiagocontelli <thiagocontellid@gmail.com>
2023-06-02 15:55:57 -0300

    refactor: change the return of user repository

--- src/main/java/com/tc/stockcontrol/user/UserRepository.java
+++ src/main/java/com/tc/stockcontrol/user/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    UserDetails findByEmail(String email);
+    User findByEmail(String email);
 }
