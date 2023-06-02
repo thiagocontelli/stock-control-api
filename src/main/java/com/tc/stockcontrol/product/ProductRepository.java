33778f927d41a21c2adfe188f7244f6f1dfd8d32
thiagocontelli <thiagocontellid@gmail.com>
2023-06-02 13:38:13 -0300

    feat: add a method to product repository to find a product by the bar code

--- src/main/java/com/tc/stockcontrol/product/ProductRepository.java
+++ src/main/java/com/tc/stockcontrol/product/ProductRepository.java
@@ -3,4 +3,6 @@ package com.tc.stockcontrol.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    Product findByBarCode(String barCode);
 }
